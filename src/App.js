3825b9b9942a8b221163cad4c2ba4a66897ad005
Angelica <puchetatome.angelica@gmail.com>
2024-06-05 13:35:00 -0600

    Added some styles to the home component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Header />
+        <Home />
       </Router>
     </div>
   );
---
+++ src/components/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import home from '../assets/images/home.jpg'
+import '../styles/Home.css'
+
+const Home = () => {
+    return (
+        <div className='mx-auto content grid grid-cols-1 md:grid-cols-2 auto-rows-fr'>
+            <div className='content-image'>
+                <img src={home} alt="Tienda" className=' w-full h-full sm:w-full' />
+            </div>
+
+            <div className="content-text flex flex-col items-center justify-center">
+                <h1 className=" title text-xl md:text-3xl text-center p-4">Your comfort, our priority: fast and reliable home delivery service.</h1>
+                {/* <Link to="/products"> */}
+                <button className="products animate-bounce text-white m-3 px-4 py-2 rounded-3xl">Ver Productos</button>
+            {/* </Link> */}
+            </div>
+
+
+        </div>
+
+
+        // <div class="bg-red-500 sm:bg-yellow-400 md:bg-green-400 lg:bg-purple-400 xl:bg-blue-400 py-2 px-4 text-center">Resize window</div>
+
+
+    );
+};
+
+export default Home;
