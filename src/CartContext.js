599ff93595ece5139fcb5a78378a3c922e25094d
Angelica <puchetatome.angelica@gmail.com>
2024-06-12 17:39:22 -0600

    Wrap the Application with the Provider.

--- src/CartContext.js
+++ src/CartContext.js
@@ -2,7 +2,7 @@ import React, { createContext, useState } from 'react';
 
 export const CartContext = createContext();
 
-export const shopCartProvider = ({ children }) => {
+export const ShopCartProvider = ({ children }) => {
 
     const [shopCart, setShopCart] = useState([]);
 
