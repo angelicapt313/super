2ffe05186940d3a29de90a68e10eb01357a3be27
Angelica <puchetatome.angelica@gmail.com>
2024-06-12 18:26:19 -0600

    Updated the product card component, and the products page with the context.

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,7 @@
 // src/components/ProductCard.jsx
 import React, { useState } from 'react';
 
-const ProductCard = ({ product }) => {
+const ProductCard = ({ product, addShopCart }) => {
     const [quantity, setQuantity] = useState(0);
 
     const imagePath = require(`../assets/images/${product.image}`);
@@ -11,10 +11,11 @@ const ProductCard = ({ product }) => {
             <img src={imagePath} alt={product.name} className="w-full h-32 object-cover mb-2" />
             <h2 className="text-lg font-bold">{product.name}</h2>
             <p className="text-gray-700">${product.price}</p>
+            
             <div className="mt-2">
                 {quantity === 0 ? (
                     <button
-                        onClick={() => setQuantity(1)}
+                        onClick={() => addShopCart(product)}
                         className="bg-green-500 text-white px-2 py-1 rounded"
                     >
                         Agregar
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,10 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useContext} from 'react';
 import ProductCard from '../components/ProductCard';
+import { CartContext } from '../CartContext';
 
 const Products = () => {
+
+    const { addShopCart } = useContext(CartContext);
 
     const [products, setProducts] = useState([]);
 
@@ -27,11 +30,13 @@ const Products = () => {
             <h1 className="text-3xl font-bold mb-6">Productos</h1>
              <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4">
                 {products.map(product => (
-                    <ProductCard key={product.id} product={product} />
+                    <ProductCard key={product.id} product={product} addShopCart={addShopCart} />
                 ))}
             </div>
         </div>
     );
 };
+
+
 
 export default Products;
