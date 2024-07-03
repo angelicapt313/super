2c2ffc646beb8ba30fee59344d77514a0eac2eaf
Angelica <puchetatome.angelica@gmail.com>
2024-07-03 11:16:08 -0600

    I've created a Logout component.

---
+++ src/components/LogoutButton.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useMsal } from "@azure/msal-react";
+
+const LogoutButton = () => {
+    const { instance } = useMsal();
+
+    const handleLogout = () => {
+        instance.logoutPopup().catch(e => {
+            console.error(e);
+        });
+    };
+
+    return <button onClick={handleLogout}>Logout</button>;
+};
+
+export default LogoutButton;
