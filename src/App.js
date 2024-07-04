20a7f018509bbce2c450143ec64ec0f343bca24e
Angelica <puchetatome.angelica@gmail.com>
2024-07-03 18:22:50 -0600

    Enabled route protection with the MSAL Authentication Template.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Products from './pages/Products';
-import Cart from './components/Cart'
+import Cart from './components/Cart';
+import { MsalAuthenticationTemplate } from '@azure/msal-react';
+import { loginRequest } from './authConfig';
+import ProtectedPage from './components/ProtectedPage';
 
 function App() {
   return (
@@ -14,6 +17,14 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/products' element={<Products />} />
           <Route path='/cart' element={<Cart />} />
+          <Route 
+            path="/protected" 
+            element={
+              <MsalAuthenticationTemplate interactionType="popup" authenticationRequest={loginRequest}>
+                <ProtectedPage />
+              </MsalAuthenticationTemplate>
+            } 
+          />
         </Routes>
       </Router>
     </div>
