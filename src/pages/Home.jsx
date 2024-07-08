4468ae6143148db7b55d1763a471b2bb6d50ae79
Angelica <puchetatome.angelica@gmail.com>
2024-07-08 12:19:51 -0600

    I've imported the Login button component.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import file from '../assets/images/file.png'
 import '../styles/Home.css'
 import Products from './Products';
+import LoginButton from '../components/LoginButton'
 
 const Home = () => {
     return (
@@ -10,7 +11,7 @@ const Home = () => {
             <div className="info-content flex flex-col lg:flex-row justify-center items-center ">
 
                 <div className="content text-center md:w-50 mt-4">
-                   
+                   <LoginButton />
                     <h1 className="title text-3xl font-bold mb-4">What you <span>want</span>, when you <span>need it </span>...</h1>
                     <p className="subtitle text-gray-600 mb-4 text-lg">Because your <span>comfort </span>, our priority: <span>fast</span> and <span>reliable</span> home delivery service!</p>
                     <Link to="/products">
