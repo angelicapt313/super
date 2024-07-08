805cf693a3fe1c972371e09a468edd0a7c82264d
Angelica <puchetatome.angelica@gmail.com>
2024-07-08 12:16:38 -0600

    I've added some classes to responsive styling.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,22 +6,26 @@ import Products from './Products';
 
 const Home = () => {
     return (
-        <div className="content">
-            <div className="flex flex-col items-center md:flex-row md:justify-between p-4 my-3">
-                <div className="md:w-1/2 p-4">
+        <div className="content h-screen flex justify-center items-center">
+            <div className="info-content flex flex-col lg:flex-row justify-center items-center ">
+
+                <div className="content text-center md:w-50 mt-4">
+                   
                     <h1 className="title text-3xl font-bold mb-4">What you <span>want</span>, when you <span>need it </span>...</h1>
                     <p className="subtitle text-gray-600 mb-4 text-lg">Because your <span>comfort </span>, our priority: <span>fast</span> and <span>reliable</span> home delivery service!</p>
                     <Link to="/products">
                         <button className="products animate-bounce text-white m-3 px-4 py-2 rounded-3xl">Ver Productos</button>
                     </Link>
                 </div>
-                <div className="md:w-1/2 p-4">
+
+                <div className="image-content w-96 md:w-50">
                     <img
                         src={file}
                         alt="Products Supermarket"
-                        className="w-full h-auto"
+                        className="w-full"
                     />
                 </div>
+
             </div>
         </div>
 
