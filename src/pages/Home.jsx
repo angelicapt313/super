3e6a647a2d00be50d0f38305b5558f3c59082ca9
Angelica <puchetatome.angelica@gmail.com>
2024-07-08 19:53:15 -0600

    I've added new responsive styling design to the home component.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,27 +3,27 @@ import { Link } from 'react-router-dom';
 import file from '../assets/images/file.png'
 import '../styles/Home.css'
 import Products from './Products';
-import LoginButton from '../components/LoginButton'
+import iconUser from '../assets/images/iconUser.png';
 
 const Home = () => {
     return (
         <div className="content h-screen flex justify-center items-center">
             <div className="info-content flex flex-col lg:flex-row justify-center items-center ">
 
-                <div className="content text-center md:w-50 mt-4">
-                   <LoginButton />
-                    <h1 className="title text-3xl font-bold mb-4">What you <span>want</span>, when you <span>need it </span>...</h1>
-                    <p className="subtitle text-gray-600 mb-4 text-lg">Because your <span>comfort </span>, our priority: <span>fast</span> and <span>reliable</span> home delivery service!</p>
+                <div className="content text-center">
+
+                    <h1 className="title text-2xl font-bold mb-4">What you <span>want</span>, when you <span>need it </span>...</h1>
+                    <p className="subtitle text-gray-600 mb-4 text-mg">Because your <span>comfort </span>, our priority: <span>fast</span> and <span>reliable</span> home delivery service!</p>
                     <Link to="/products">
                         <button className="products animate-bounce text-white m-3 px-4 py-2 rounded-3xl">Ver Productos</button>
                     </Link>
                 </div>
 
-                <div className="image-content w-96 md:w-50">
+                <div className="image-content">
                     <img
                         src={file}
                         alt="Products Supermarket"
-                        className="w-full"
+                        className="w-96 lg:w-100"
                     />
                 </div>
 
