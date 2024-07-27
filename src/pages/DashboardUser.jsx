6f31b881c0c6513e101b8e18db6f5f8f3f707819
Diego Ochoa Maldonado <diego.ochoa.maldonado@hotmail.com>
2024-07-27 16:24:58 -0600

    updated grid view on Dashboarduser

--- src/pages/DashboardUser.jsx
+++ src/pages/DashboardUser.jsx
@@ -13,7 +13,7 @@ const DashboardUser = () => {
      
             <div className="flex-1 p-6">
             <h2 className="text-2xl font-bold mb-6">Main Dashboard</h2>
-            <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {<Charts></Charts>}
                 {<Charts></Charts>}
                 {<Charts></Charts>}
