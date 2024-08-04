14741030b5c78eb1980822332e9fff976c7b4fa9
Diego Ochoa Maldonado <diego.ochoa.maldonado@hotmail.com>
2024-08-04 16:52:05 -0600

    removed unused yml files

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,4 +1,7 @@
+
 export const msalConfig = {
+
+    
     auth: {
         clientId: "3b69122c-b10b-4af3-b8ad-5bdf35144df9",
         authority: "https://login.microsoftonline.com/27d76b99-0ce0-463e-b4b5-d9cc9e9910a8",
